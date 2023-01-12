b9610ca19bdeb074b398c6120f94ddc424dc9c29
raitis <raitisbe@gmail.com>
2023-01-12 17:57:11 +0200

    feat: Initial chat window mockup

---
+++ client/src/block.tsx
@@ -0,0 +1,59 @@
+import { Avatar, Chip, Grid, Paper } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { ReactNode } from 'react';
+
+type BlockDataModel = {
+  sender: string;
+  messages: {
+    id: string;
+    text: string;
+  }[];
+};
+
+const Message = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#d9e3f7',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  margin: theme.spacing(0.5),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+/**
+ * Messages coming from one sender are grouped into blocks
+ * @param props 
+ * @returns 
+ */
+export function Block(props: { data: BlockDataModel; }) {
+  /**
+   * Get the first letter of sender to display in the chip
+   * @param sender 
+   * @returns 
+   */
+  function firstLetter(sender: string): ReactNode {
+    if (sender && sender.length > 0) {
+      return sender[0].toUpperCase();
+    }
+  }
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={2} md={2}>
+        <Chip
+          avatar={<Avatar>{firstLetter(props.data.sender)}</Avatar>}
+          label={props.data.sender}
+          variant="outlined" />
+      </Grid>
+      <Grid item xs={12} md={10}>
+        {props.data.messages.map((msg, i) => {
+          return (
+            <Grid item key={i}>
+              <Message>{msg.text}</Message>
+            </Grid>
+          );
+        })}
+      </Grid>
+
+    </Grid>
+  );
+}
--- client/src/chat.tsx
+++ client/src/chat.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import { Grid, IconButton, TextField } from '@mui/material';
+import { Block } from './block';
+import SendIcon from '@mui/icons-material/Send';
+
+const blocks = [
+  {
+    sender: 'raitis',
+    messages: [{id: 'aaa', text: 'aaaaa'}, {id: 'aaa2', text: 'zzz'}],
+  },
+  {
+    sender: 'maija',
+    messages: [{id: 'aaa', text: 'fasdsds'}],
+  },
+]
+
+const SendButton = () => (
+  <IconButton>
+    <SendIcon />
+  </IconButton>
+  )
 
 export function Chat() {
   return (
     <div>
-      <h2>Chat</h2>
+      <Grid container justifyContent={'center'} spacing={2}>
+        <Grid item xs={12} md={8}>
+        {blocks.map((block, i) => {
+          return (
+            <Block data={block}></Block>
+          )
+       })
+      }
+      </Grid>
+        <Grid item xs={12} md={8}>
+          <TextField label="Message:" variant="outlined" fullWidth InputProps={{endAdornment: <SendButton />}}/>
+        </Grid>
+      </Grid>
     </div>
   );
 }
+
+
+
