d848fa3cf2fa852568c8a911794bf4d9c11ab27a
raitis <raitisbe@gmail.com>
2023-01-12 15:35:37 +0200

    feat: Add sign in form ui

--- client/src/landing.tsx
+++ client/src/landing.tsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 export function Landing() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  /*  event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('username')
+    });
+    */
+  };
+
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Welcome to chat
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Enter
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
