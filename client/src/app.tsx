9e6daacabe2c5ce4cf01c785cc22b0044b8c6f59
raitis <raitisbe@gmail.com>
2023-01-12 15:25:18 +0200

    feat: Add router and align filenames

---
+++ client/src/app.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import './app.css';
+import { Chat } from './chat';
+import { Landing } from './landing';
+import { Layout } from './layout';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="chat" element={<Chat />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+      
+    </div>
+  );
+}
+
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ client/src/chat.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Chat() {
+  return (
+    <div>
+      <h2>Chat</h2>
+    </div>
+  );
+}
