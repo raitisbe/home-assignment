6780810319c59b83b0ae281a2dc00daed3b2a085
raitis <raitisbe@gmail.com>
2023-01-13 17:24:47 +0200

    feat: Make chat protected

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import React, { ReactNode } from 'react';
+import { Routes, Route, Link, useNavigate, RouteProps, Navigate, Outlet } from "react-router-dom";
 import './app.css';
 import { Chat } from './chat';
 import { Landing } from './landing';
 import { Layout } from './layout';
+import { socketService } from './sockets';
+
+interface Props {
+  children?: JSX.Element
+}
+
+const ProtectedRoute = ({ children, ...props }: Props) => {
+  if (!socketService) {
+    console.warn('Redirecting due to socketService', socketService)
+    return <Navigate to="/" replace />;
+  }
+
+  if (!socketService.ws) {
+    console.warn('Redirecting due to socketService.ws', socketService.ws)
+    return <Navigate to="/" replace />;
+  }
+
+  if (socketService.ws.readyState !== WebSocket.OPEN) {
+    console.warn('Redirecting due to readyState', socketService.ws.readyState)
+    return <Navigate to="/" replace />;
+  }
+
+  return children ? children : <Outlet />;
+};
 
 function App() {
   const navigation = useNavigate();
@@ -12,8 +36,15 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Landing />} />
-          <Route path="chat" element={<Chat navigation={navigation} />} />
+          <Route index element={<Landing navigate={navigation}/>} />
+          <Route
+          path="chat"
+          element={
+            <ProtectedRoute>
+              <Chat navigate={navigation} />
+            </ProtectedRoute>
+          }
+        />
 
           {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
