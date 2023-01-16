ac105041f1b4e06a753c3ce27f9d70ab59966484
raitis <raitisbe@gmail.com>
2023-01-16 13:05:28 +0200

    feat: Improve top menu

--- client/src/layout.tsx
+++ client/src/layout.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Box from '@mui/material/Box';
+import { Link } from '@mui/material';
+import {Link as LinkRR} from 'react-router-dom';
 
 export function Layout() {
   return (
     <div>
       {/* A "layout route" is a good place to put markup you want to
                 share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/chat">Chat</Link>
-          </li>
-        </ul>
-      </nav>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <Link component={LinkRR} to="/" sx={{minWidth : 100 }}>Home</Link>
+        <Link component={LinkRR} to="/chat" sx={{minWidth : 100 }}>Chat</Link>
+      </Box>
 
       <hr />
 
-      {/* An <Outlet> renders whatever child route is currently active,
-                so you can think about this <Outlet> as a placeholder for
-                the child routes we defined above. */}
+
       <Outlet />
     </div>
   );
